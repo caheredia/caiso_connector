23af9ecd4bb50f647f94f6ff399ce40831f121ac
Cristian <cheredia@kountable.com>
2019-12-28 12:57:14 -0800

    refine column selection

--- src/input/caiso_connector.py
+++ src/input/caiso_connector.py
@@ -3,9 +3,9 @@ from urllib.request import urlretrieve
 import os
 
 BASE_URL = "http://oasis.caiso.com/oasisapi/SingleZip"
-query_name = "?queryname=PRC_INTVL_LMP"
+query_name = "?queryname=PRC_LMP"
 time_range = "&startdatetime=20190201T00:00-0000&enddatetime=20190206T00:00-0000"
-query_params = "&version=1&market_run_id=RTM&grp_type=ALL_APNODES&resultformat=6"
+query_params = "&version=1&market_run_id=DAM&grp_type=ALL_APNODES&resultformat=6"
 # build full caiso url
 target_url = BASE_URL + query_name + time_range + query_params
 ZIP_DIRECTORY = 'src/input/data'

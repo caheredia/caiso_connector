3d59f29717d47339c76bf186f835a3a8f861a068
Cristian <cheredia@kountable.com>
2019-12-28 12:38:05 -0800

    add notebook

--- src/input/caiso_connector.py
+++ src/input/caiso_connector.py
@@ -1,6 +1,5 @@
 import zipfile
 from urllib.request import urlretrieve
-import pandas as pd
 import os
 
 BASE_URL = "http://oasis.caiso.com/oasisapi/SingleZip"
@@ -9,7 +8,6 @@ time_range = "&startdatetime=20190201T00:00-0000&enddatetime=20190206T00:00-0000
 query_params = "&version=1&market_run_id=RTM&grp_type=ALL_APNODES&resultformat=6"
 # build full caiso url
 target_url = BASE_URL + query_name + time_range + query_params
-print(target_url)
 ZIP_DIRECTORY = 'src/input/data'
 
 
@@ -71,18 +69,5 @@ def delete_data_files(search_directory: str):
     for temp_file in os.listdir(search_directory):
         os.remove(os.path.join(search_directory, temp_file))
 
-
 # "http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_INTVL_LMP&startdatetime=20190201T00:00-0000&enddatetime=20190206T00:00-0000&version=1&market_run_id=RTM&grp_type=ALL_APNODES&resultformat=6
 # "http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime=20190201T00:00-0000&enddatetime=20190203T00:00-0000&version=1&market_run_id=DAM&grp_type=ALL_APNODES&resultformat=6
-
-
-if __name__ == "__main__":
-    download_csv_file(target_url, ZIP_DIRECTORY)
-    unzip_csv(ZIP_DIRECTORY)
-
-    for file in find_csv_files(ZIP_DIRECTORY):
-        df = pd.read_csv(file)
-        print(df.head())
-    print(df.columns)
-
-    delete_data_files(ZIP_DIRECTORY)

1e5848cc6fc9b330c93f03f06ca0b211d6cf4b32
Cristian <cheredia@kountable.com>
2019-12-31 19:57:22 -0800

    refactor url generation

---
+++ src/etl.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import sqlite3
+from datetime import timedelta
+
+from src.helpers import ZIP_DIRECTORY, generate_url
+
+from src.caiso_connector import (
+    download_csv_file,
+    unzip_csv,
+    delete_data_files,
+    find_csv_files
+)
+
+DATABASE_FILENAME = "src/output/lmp.db"
+
+# Connect to database
+conn = sqlite3.connect(DATABASE_FILENAME)
+
+if __name__ == "__main__":
+    # Extract
+    for date in pd.date_range("2019-10-01", "2019-11-29"):
+        start_time = date.isoformat()[:-3].replace('-', '')
+        end_time = (date + timedelta(days=1)).isoformat()[:-3].replace('-', '')
+        target = generate_url(start_time, end_time)
+        download_csv_file(target, ZIP_DIRECTORY)
+        unzip_csv(ZIP_DIRECTORY)
+
+        # Transform
+        lmp_columns = ['INTERVALSTARTTIME_GMT', 'NODE', 'LMP_TYPE', 'MW']
+        file = find_csv_files(ZIP_DIRECTORY)[0]
+        df = (pd.read_csv(file, usecols=lmp_columns)
+              .rename(columns={'INTERVALSTARTTIME_GMT': 'time', 'NODE': 'node', 'MW': 'mw'}))
+        df.time = pd.to_datetime(df.time)
+        df = df[df["LMP_TYPE"] == "LMP"].drop(columns=['LMP_TYPE'])
+
+        # Load
+        print(f"adding {start_time}")
+        df.to_sql("lmp", conn, if_exists="append", index=False)
+
+        delete_data_files(ZIP_DIRECTORY)
--- src/helpers.py
+++ src/helpers.py
@@ -2,9 +2,26 @@ DATABASE_LOCATION = "src/lmp.db"
 ZIP_DIRECTORY = 'src/temp_data'
 
 
-BASE_URL = "http://oasis.caiso.com/oasisapi/SingleZip"
-query_name = "?queryname=PRC_LMP"
-time_range = "&startdatetime=20190201T00:00-0000&enddatetime=20190206T00:00-0000"
-query_params = "&version=1&market_run_id=DAM&grp_type=ALL_APNODES&resultformat=6"
-# build full caiso url
-target_url = BASE_URL + query_name + time_range + query_params
+def generate_url(start_time: str, end_time: str):
+    """Generates a CAISO OASIS url based on start and time.
+
+    Parameters
+    ----------
+    start_time : str
+        start timestamp
+    end_time : str
+        end timestamp
+
+    Returns
+    -------
+    target_url: str
+        OASIS url for download
+    """
+
+    base_url = "http://oasis.caiso.com/oasisapi/SingleZip"
+    query = "?queryname=PRC_LMP"
+    time_range = f"&startdatetime={start_time}-0000&enddatetime={end_time}-0000"
+    version = "&version=1&market_run_id=DAM&grp_type=ALL_APNODES&resultformat=6"
+
+    target_url = base_url + query + time_range + version
+    return target_url

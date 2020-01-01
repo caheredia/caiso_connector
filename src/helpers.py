1427ad89b2a1d58eb2b6d0ceac2c465dd009c930
Cristian <cheredia@kountable.com>
2019-12-31 19:41:41 -0800

    update tests

--- src/helpers.py
+++ src/helpers.py
@@ -8,5 +8,3 @@ time_range = "&startdatetime=20190201T00:00-0000&enddatetime=20190206T00:00-0000
 query_params = "&version=1&market_run_id=DAM&grp_type=ALL_APNODES&resultformat=6"
 # build full caiso url
 target_url = BASE_URL + query_name + time_range + query_params
-
-print(target_url)

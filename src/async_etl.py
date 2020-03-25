8bec2702578165218719281bfd0842553d0c54e9
cheredia <cheredia@wattlearn.com>
2020-03-24 18:26:14 -0700

    refactor async etl

--- src/async_etl.py
+++ src/async_etl.py
@@ -1,42 +1,37 @@
 import asyncio
 from datetime import timedelta
-import time
+
 import aiofiles
-import aiohttp
 import pandas as pd
-from src.helpers import generate_url
+from aiohttp import ClientSession
+from src.constants import IBM_API_KEY, SAVE_FOLDER
 
 # generate links
 caiso_urls = {}
-for date in pd.date_range("2019-01-01", "2019-01-02"):
-    start_time = date.isoformat()[:-3].replace('-', '')
-    end_time = (date + timedelta(days=1)).isoformat()[:-3].replace('-', '')
-    caiso_urls[start_time] = generate_url(start_time, end_time)
+for date in pd.date_range("2019-01-01", "2019-01-03"):
+    start_time = date.isoformat()[:-3].replace('-', '').split('T')[0]
+    end_time = (date + timedelta(days=1)).isoformat()[:-3].replace('-', '').split('T')[0]
+    caiso_urls[start_time] = f"http://api.weather.com/v3/wx/hod/conditions/historical/point?format=json&units=s&pointType=nearest&geocode=33.0917%2C-95.0417&startDateTime={start_time}0000&endDateTime={end_time}0000&apiKey={IBM_API_KEY}"
 
 
-async def download(url: str, filename: str) -> None:
+async def download(url: str, filename: str, session: ClientSession) -> None:
     """Downloads caiso zip file."""
     print(f"starting {filename}")
-    time.sleep(10)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            print("header---", resp.headers)
-            #data = await resp.text()
-            #print("content----", data)
+    resp = await session.get(url)
+    resp.raise_for_status()
+    content = await resp.content.read()
 
-            resp.raise_for_status()
-            content = await resp.content.read()
-
-        async with aiofiles.open("src/temp_data/" + filename + ".zip", 'wb') as f:
-            await f.write(content)
-            print(f"finished {filename}")
+    async with aiofiles.open(SAVE_FOLDER + filename + ".json", 'wb') as f:
+        await f.write(content)
+        print(f"finished {filename}")
 
 
 async def main():
-    coroutines = []
-    for url in caiso_urls:
-        coroutines.append(download(caiso_urls.get(url), url))
-    await asyncio.gather(*coroutines)
+    async with ClientSession() as session:
+        coroutines = []
+        for url in caiso_urls:
+            coroutines.append(download(caiso_urls.get(url), url, session))
+        await asyncio.gather(*coroutines)
 
 
 if __name__ == '__main__':

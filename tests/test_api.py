e6c9f8f4fc897e7de62bf95cb27aad61e38409d3
cheredia <cheredia@wattlearn.com>
2020-05-13 14:22:32 -0700

    format spacing

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_get_row_count():
     response = client.get("/row-count")
     assert response.status_code == 200
-    assert isinstance(response.json().get('row-count'), int)
+    assert isinstance(response.json().get("row-count"), int)
 
 
 def test_get_time_ranges():
@@ -21,7 +21,7 @@ def test_get_time_ranges():
 def test_lmp_no_region():
     response = client.get("/lmp")
     assert response.status_code == 200
-    assert isinstance(response.json().get('regions'), list)
+    assert isinstance(response.json().get("regions"), list)
 
 
 def test_lmp_no_region_bad_response():
@@ -33,10 +33,11 @@ def test_lmp_mean():
     response = client.get("/lmp/mean/{region}/{day_of_week}")
     assert response.status_code == 422
 
-def add_int(x,y):
+
+def add_int(x, y):
     return x + abs(y)
 
-@given(strategy.integers(), strategy.integers())
-def test_add(x, y ):
-    assert add_int(x, y) == x + y
 
+@given(strategy.integers(), strategy.integers())
+def test_add(x, y):
+    assert add_int(x, y) == x + y

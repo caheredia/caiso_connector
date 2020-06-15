64aee3bb1ce27ec5bd6826aa942f9cfd1b90ee07
Cristian Heredia <cheredia@wattlearn.com>
2020-06-14 19:38:48 -0700

    config file for database loading

---
+++ app/config.py
@@ -0,0 +1,10 @@
+# used to set database location 
+from pydantic import BaseSettings
+from src.helpers import DATABASE_LOCATION
+
+
+class Settings(BaseSettings):
+    database: str = DATABASE_LOCATION
+
+
+settings = Settings()

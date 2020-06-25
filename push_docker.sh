99b487088cb76849dc84cebc7a5be047a0b3501d
Cristian Heredia <cheredia@wattlearn.com>
2020-06-24 22:54:37 -0700

    add push command

--- push_docker.sh
+++ push_docker.sh
@@ -6,4 +6,6 @@ GIT_COMMIT=$(git rev-parse HEAD)
 docker build \
        -t "caheredia/caiso-connector:${GIT_COMMIT}" \
        -f Dockerfile .
+
+docker push "caheredia/caiso-connector:${GIT_COMMIT}"
        

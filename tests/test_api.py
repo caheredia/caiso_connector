0bf77a33ff288a61ca7a4a92d791b4d3e0f81ec2
Cristian Heredia <cheredia@wattlearn.com>
2020-06-24 22:32:43 -0700

    adjust mean_lmp for test db

--- tests/test_api.py
+++ tests/test_api.py
@@ -26,6 +26,12 @@ def test_lmp_no_region():
     assert isinstance(response.json().get("regions"), list)
 
 
+def test_lmp_with_region():
+    response = client.get("/lmp/AFPR_1_TOT_GEN-APND")
+    assert response.status_code == 200
+    assert isinstance(response.json()[0], dict)
+
+
 def test_lmp_no_region_bad_response():
     response = client.get("/lmp/")
     assert response.status_code == 404
@@ -34,6 +40,12 @@ def test_lmp_no_region_bad_response():
 def test_lmp_mean():
     response = client.get("/lmp/mean/{region}/{day_of_week}")
     assert response.status_code == 422
+
+
+def test_lmp_mean_with_node():
+    response = client.get("/lmp/mean/AFPR_1_TOT_GEN-APND/monday")
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
 
 
 def test_get_db_location():

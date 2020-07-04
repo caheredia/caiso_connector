86ef270626bf7502b484bb9eb367e8b7590227eb
Cristian Heredia <cheredia@wattlearn.com>
2020-07-04 00:22:31 -0700

    replace conn with engine

--- scratch.py
+++ scratch.py
@@ -1,6 +1,7 @@
 import datetime
 from sql_app import models
 from sql_app.database import engine, SessionLocal
+import pandas as pd
 
 models.Base.metadata.create_all(bind=engine)
 db = SessionLocal()
@@ -8,11 +9,12 @@ db_lmp = models.Lmp(time=datetime.datetime.now(), node="foo", mw=1.2)
 db.add(db_lmp)
 db.commit()
 db.refresh(db_lmp)
-print(models.Base.metadata.tables)
-print(db.query(models.Lmp.node).all())
+# print(models.Base.metadata.tables)
+# print(db.query(models.Lmp.node).all())
 # TODO db query returns class objects, create new database from csv!
 for user in db.query(models.Lmp)[0:3]:
     print(user.time)
     print(user.node)
     print(user.mw)
-
+count = pd.read_sql_query("""select COUNT(*) from lmp;""", engine).values[0][0]
+print(count)

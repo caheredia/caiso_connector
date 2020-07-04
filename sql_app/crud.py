c2960b12214512f87e3d06e48bc8a8661100950b
Cristian Heredia <cheredia@wattlearn.com>
2020-07-04 00:48:07 -0700

    remove unused function

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,16 +1,7 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models
 
 
 def get_lmps(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Lmp).offset(skip).limit(limit).all()
-
-
-def bulk_create_lmp(db: Session, csv_file: str):
-    """Take a csv file as input and bulk loads rows."""
-    db_lmp = models.Lmp()
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item

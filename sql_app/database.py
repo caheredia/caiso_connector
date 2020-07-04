20e5e0ef8a6bf6654555382a54d90b09e2afeb60
Cristian Heredia <cheredia@wattlearn.com>
2020-07-04 00:35:13 -0700

    refactor engine

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,11 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from src.helpers import get_db_location
 
-SQLALCHEMY_DATABASE_URL = get_db_location()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(get_db_location(), connect_args={"check_same_thread": False})
 # check_same_thread only used for SQLite, https://fastapi.tiangolo.com/tutorial/sql-databases/
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

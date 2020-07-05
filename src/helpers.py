8b0399783ac54a88c56607366bcffef16674d688
Cristian Heredia <cheredia@wattlearn.com>
2020-07-04 17:31:21 -0700

    test move_file function

--- src/helpers.py
+++ src/helpers.py
@@ -4,6 +4,7 @@ DATABASE_LOCATION = "sqlite:///src/lmp.db"
 TEST_DATABASE_LOCATION = "sqlite:///tests/test_lmp.db"
 ZIP_DIRECTORY = "src/temp_data"
 SEED_FILE = "src/data/seed.csv"
+DATA_DIR = "src/data"
 
 
 def get_db_location() -> str:
@@ -40,3 +41,12 @@ def generate_url(start_time: str, end_time: str) -> str:
 
     target_url = base_url + query + time_range + version
     return target_url
+
+
+def move_file(file_path: str, new_directory: str = DATA_DIR) -> None:
+    """
+    Move file into desired directory.
+    """
+    file_name = os.path.split(file_path)[-1]
+    new_path = os.path.join(new_directory, file_name)
+    os.rename(file_path, new_path)
